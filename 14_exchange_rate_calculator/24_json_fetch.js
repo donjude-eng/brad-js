5945d6dd75743d7dcfb98908c6086106ef81343f
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-07-07 00:36:31 +0530

    Exchange Rate Calculator

---
+++ 14_exchange_rate_calculator/24_json_fetch.js
@@ -0,0 +1,9 @@
+function calculate() {
+    // fetch('24_json_fetch.json')
+    // .then(res => res.json())
+    // .then(data => (document.body.innerHTML = data[0].text));
+
+    ///The above is the basic idea behind the JSON Fetch.
+}
+
+calculate();
