af0e9c921f1f0e8354086682ef15d6a17c022159
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-07-07 00:38:23 +0530

    Dom Array Method

---
+++ 15_dom_array_methods/29_output_user_foreach.js
@@ -0,0 +1,70 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+//Fetch random user and add money
+
+async function getRandomUser() {
+    const res = await fetch ('https://randomuser.me/api');
+    const data = await res.json();
+
+    //console.log(data);
+
+    const user = data.results[0];
+
+
+    //Constructor
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)  /// Can somebody explain this once?
+
+    };
+    //console.log(user.name.first)
+
+    console.log(newUser);
+    addData(newUser)
+}
+
+//Add new onject to data arr
+
+function addData(obj) {
+    data.push(obj);// Added to the array
+
+    updateDom(data);
+}
+
+//Update Dom
+function updateDom(providedData = data) { //Why provideddata here?
+    // Clear main div
+    main.innerHTML= '<h2><strong>Person</strong>Wealth</h2>';
+
+    providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+        main.appendChild(element);
+
+        //console.log(item.name)
+    }); /// What is the relation with getRandomUser here?
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+    return '₹' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+};
+
+// Event Listners
+addUserBtn.addEventListener('click' , getRandomUser);
+
+
+
