d8d37cddd10754a557b50a14599843e633666ff5
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-07-17 01:17:54 +0530

    Some basics of Programming

---
+++ 000Program-Testing/program-init.js
@@ -0,0 +1,60 @@
+//Addevent listeners.
+
+
+//First test
+
+// const sayHello1 = function(name1 , name2){
+//     console.log('Hello Greetings to Done');
+// }
+// sayHello1('Don' , 'Jude');
+// console.log(`${name1} , ${name2}`);
+
+// let sayHello = function(name1 , name2) {
+//     console.log('Hello greetings to');
+//     console.log(`${name1} , ${name2}`)
+// }
+
+// sayHello('Don', 'Jude');
+
+// let fullNameMaker = function(firstName, lasteName) {
+//     console.log('Welcome to Dons world');
+//     console.log(`Happy to have you,${firstName} ${lasteName}`);
+// }
+
+// fullNameMaker('Don' , 'Jude');
+
+
+// let numAdd = function(num1 , num2) {
+//     let added = num1 + num2;
+//     return added
+// }
+
+// let total = numAdd (50 , 75);
+// console.log(total)
+
+// //console.log(numAdd(50 , 25));
+
+
+// let nameConstruct = function(name1, name2 , name3) {
+//     console.log('Welcome to my home, MR')
+//     console.log(`${name1} ${name2} ${name3}`)
+// }
+
+//nameConstruct('Sagar' , 'Alias' , 'Jacky')
+
+// let nameConstruct2 = function(name1 , name2) {
+//     let added = name1 + name2
+//     return nameConstruct
+// }
+
+// console.log(nameCOnstruct2('Don Jude'));
+
+
+let addNum = function(number1 , number2) {
+    let value1 = number1 + number2;
+    let value2 = number1 - number2;
+    return value1 , value2
+}
+
+let result = addNum(10 , 20)
+console.log(result)
