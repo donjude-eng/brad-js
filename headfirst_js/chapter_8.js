0de40d926c82cb8bd133d170f7ece489e50a6037
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-08-11 11:16:20 +0530

    Chapter-8

---
+++ headfirst_js/chapter_8.js
@@ -0,0 +1,26 @@
+let view = {
+    displayMessage: function (msg) {
+        let messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function (location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+    displayMiss: function (location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    }
+
+};
+
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+view.displayMessage("Tap tap, is this thing on?");
+
+
+console.log(messageArea);
