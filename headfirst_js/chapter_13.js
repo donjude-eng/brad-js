dcbf4328fe470f84079fbb9ce4213f5693b26bfd
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-08-21 01:01:44 +0530

    Chapter 13 updated

--- headfirst_js/chapter_13.js
+++ headfirst_js/chapter_13.js
@@ -4,9 +4,9 @@ function Dog(name, breed, weight) {
     this.weight = weight;
     this.bark = function () {
         if (this.weight > 25) {
-            alert(this.name + " says Woof!");
+            console.log(this.name + " says Woof!");
         } else {
-            alert(this.name + " says Yip!");
+            console.log(this.name + " says Yip!");
         }
     };
 }
@@ -14,3 +14,59 @@ function Dog(name, breed, weight) {
 let fido = new Dog("Fido", "Mixed", 38);
 let fluffy = new Dog("Fluffy", "Poodle", 30);
 let spot = new Dog("Spot", "Chihuahua", 10);
+console.log(fido.bark());
+
+//How to set up the prototype
+function Dog(name, breed, weight) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+}
+
+Dog.prototype.species = "Canine";
+Dog.prototype.bark = function () {
+    if (this.weight > 25) {
+        console.log(this.name + " says Woof!");
+    } else {
+        console.log(this.name + " says Yip!");
+    }
+};
+Dog.prototype.run = function () {
+    console.log("Run!");
+};
+Dog.prototype.wag = function () {
+    console.log("Wag!");
+};
+
+//Test drive the prototype with some dogs
+// function Dog(name, breed, weight) {
+//     this.name = name;
+//     this.breed = breed;
+//     this.weight = weight;
+// }
+// Dog.prototype.species = "Canine";
+// Dog.prototype.bark = function () {
+//     if (this.weight > 25) {
+//         console.log(this.name + " says Woof!");
+//     } else {
+//         console.log(this.name + " says Yip!");
+//     }
+// };
+// Dog.prototype.run = function () {
+//     console.log("Run!");
+// };
+// Dog.prototype.wag = function () {
+//     console.log("Wag!");
+// };
+// var fido = new Dog("Fido", "Mixed", 38);
+// var fluffy = new Dog("Fluffy", "Poodle", 30);
+// var spot = new Dog("Spot", "Chihuahua", 10);
+// fido.bark();
+// fido.run();
+// fido.wag();
+// fluffy.bark();
+// fluffy.run();
+// fluffy.wag();
+// spot.bark();
+// spot.run();
+// spot.wag();
