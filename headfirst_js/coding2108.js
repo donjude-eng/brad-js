5b5f1c48c649054a4b40d1179f3b807956da4a54
donjude-eng <donjudejoseph123@gmail.com>
2020-08-25 11:29:53 +0530

    Chapter9

--- headfirst_js/coding2108.js
+++ headfirst_js/coding2108.js
@@ -25,3 +25,31 @@ const convertDigitToWords = (number) => {
 };
 let number = 12387;
 console.log(convertDigitToWords(number))
+
+const convertDigitToWords = (number) => {
+    let numberInWords = "";
+    let words = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    while (number > 0) {
+        let digit = number % 10;
+        for (let i = 0; i < words.length; i++) {
+            if (i === digit) {
+                numberInWords = words[i] + " " + numberInWords;
+            }
+        }
+        number = Math.floor(number / 10);
+    }
+    return numberInWords;
+};
+let number = 12387;
+console.log(convertDigitToWords(number))
