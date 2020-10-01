7ea75e49f18ca8338e8c3bfa5f6e2b3f57cdcd4d
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-10-01 11:27:46 +0530

    upadted

--- 29_breakout_game/29_breakout_game.js
+++ 29_breakout_game/29_breakout_game.js
@@ -6,6 +6,10 @@ const ctx = canvas.getContext("2d");
 
 let score = 0;
 
+const brickRowCount = 9;
+const brickColoumnCount = 5;
+
+
 //Create ball props
 const ball = {
   x: canvas.width / 2,
@@ -26,6 +30,29 @@ const paddle = {
   dx: 0,
 };
 
+// Create brick props
+const brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY:60,
+    visible: true
+}
+
+//Create bricks
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++){
+    bricks[i] = [];
+    for(let j = 0; j < brickColoumnCount; j++){
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+        bricks[i] [j] = { x, y, ... brickInfo}
+
+
+    }
+}
+
 //Draw ball on canvas
 function drawBall() {
   ctx.beginPath();
@@ -44,18 +71,36 @@ function drawPaddle() {
   ctx.closePath();
 }
 
-// Draw everything
-function draw() {
-  drawBall();
-  drawPaddle();
-  drawScore()
-}
 
 // Draw score on canvas
 function drawScore(){
     ctx.font = '20px Arial';
     ctx.fillText(`Score: ${score}`, canvas.width - 100, 30)
 }
+
+
+console.log(bricks)
+//Draw bricks on canvas
+function drawBricks(){
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            ctx.beginPath();
+            ctx.rect(brick.x, brick.y, brick.w, brick.h);
+            ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent';
+            ctx.fill();
+            ctx.closePath();
+        })
+    })
+}
+
+// Draw everything
+function draw() {
+    drawBall();
+    drawPaddle();
+    drawScore()
+    drawBricks();
+  }
+
 draw();
 
 //Rules and close event listners
